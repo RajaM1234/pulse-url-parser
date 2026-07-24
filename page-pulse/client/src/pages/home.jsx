cec3c4aeba9811af9959f1765ddd216a52c67da5
RajaM1234 <raja4507r@gmail.com>
2026-07-24 20:59:07 +0530

    added the credentials in the footer

--- page-pulse/client/src/pages/home.jsx
+++ page-pulse/client/src/pages/home.jsx
@@ -25,6 +25,16 @@ const Home = () => {
             {error && <ErrorCard message={error} />}
 
             {report && <ReportCard report={report} />}
+            <footer className="footer">
+                Built for{" "}
+                <a
+                    href="https://digitalheroesco.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Digital Heroes Training Task
+                </a>
+            </footer>
         </div>
     );
 };
