2b35fd6b236cffdd8a97a0e23c31eba4a720998d
RajaM1234 <raja4507r@gmail.com>
2026-07-24 20:11:18 +0530

    Seted up the fronted for the page pulse

---
+++ page-pulse/client/src/pages/home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Page Pulse</h1>
+            <p>Analyze any website instantly.</p>
+        </div>
+    );
+};
+
+export default Home;
